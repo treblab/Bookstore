98217c5f85394d6e0e5b15ffd1f32d3c9b758139
treblab <albertnguyen2002@hotmail.com>
2024-08-02 17:58:47 +1000

    Setup of MongDB
    -Connected MongoDB database to project
    -Created Book model/schema

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://albertnguyen2002TjYCNj2Z4oS2fIIS:Bloopblarp1!@bookstore.dl6gmhz.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Bookstore'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,52 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import {Book} from './models/bookModel.js';
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+// Middleware for parsing request body - using Postman.
+app.use(express.json());
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Welcome to MERN!');
 });
+
+// Route for saving a new book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        // Send newly created book to database
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    })
