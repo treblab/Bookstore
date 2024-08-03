8bb632cdf4d330bb39a07c3d6fe75937a9cdb706
treblab <albertnguyen2002@hotmail.com>
2024-08-03 15:40:13 +1000

    Update book feature added

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.get('/', (request, response) => {
     return response.status(234).send('Welcome to MERN!');
 });
 
-// Route for saving a new book
+// Route for saving a new book to the database
 app.post('/books', async (request, response) => {
     try {
         if (
@@ -39,6 +39,64 @@ app.post('/books', async (request, response) => {
         response.status(500).send({message: error.message});
     }
 });
+
+// Route to get all books from the database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route for getting one book from the database
+app.get('/books/:id', async (request, response) => {
+    try {
+        
+        const {id} = request.params;
+        
+        const book = await Book.findById(id);
+        
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route for updating a book in the database
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({message: 'Book not found'});
+        }
+        
+        return response.status(200).send({message: 'Book updated successfully!'});
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
