20efa04075369f19dbaddc4c0861b668f2e0d951
treblab <albertnguyen2002@hotmail.com>
2024-08-03 16:39:37 +1000

    Initialised frontend of the project
    Initalised using Vite and React.
    
    - Setup Vite, React and tailwindcss.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='bg-red-500 text-white-500'>Welcome to the frontend of the bookstore!</div>
+  );
+}
+export default App;
