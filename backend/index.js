5d8e4b8dc5ee0987bb9794c1cfd682591340e9c0
treblab <albertnguyen2002@hotmail.com>
2024-08-03 16:03:19 +1000

    Refactored Node.js with an express router
    Refactored code to scale with different schema types of books.
    
    Changes made:
    - Moved routes into a new js file, 'bookRoutes.js'
    - Refactored and simplified code in index.js.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import {Book} from './models/bookModel.js';
+import booksRouter from './routes/bookRoutes.js';
 
 const app = express();
 
@@ -9,109 +10,10 @@ const app = express();
 app.use(express.json());
 app.get('/', (request, response) => {
     console.log(request);
-    return response.status(234).send('Welcome to MERN!');
+    return response.status(234).send('Welcome to the backend of the bookstore!');
 });
 
-// Route for saving a new book to the database
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        // Send newly created book to database
-        const book = await Book.create(newBook);
-        return response.status(201).send(book);
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route to get all books from the database
-app.get('/books', async (request, response) => {
-    try {
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        });
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route for getting one book from the database
-app.get('/books/:id', async (request, response) => {
-    try {
-        
-        const {id} = request.params;
-        
-        const book = await Book.findById(id);
-        
-        return response.status(200).json(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route for updating a book in the database
-app.put('/books/:id', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-
-        const { id } = request.params;
-        const result = await Book.findByIdAndUpdate(id, request.body);
-
-        if (!result) {
-            return response.status(404).json({message: 'Book not found'});
-        }
-        
-        return response.status(200).send({message: 'Book updated successfully!'});
-        
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route for deleting a book from the database
-app.delete('/books/:id', async (request, response) => {
-    try {
-        const {id} = request.params;
-        const result = await Book.findByIdAndDelete(id);
-        
-        if (!result) {
-            return response.status(404).json({message: 'Book not found'});
-        }
-        return response.status(200).send({message: 'Book deleted successfully!'});
-    } catch (error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
+app.use('/books', booksRouter);
 
 mongoose
     .connect(mongoDBURL)
